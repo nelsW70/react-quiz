c0538de30a140ca7fc7f3861308eb26adf957dc1
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-17 14:23:16 -0500

    add: next question basic functionality

--- src/context.js
+++ src/context.js
@@ -46,6 +46,18 @@ const AppProvider = ({ children }) => {
     }
   };
 
+  const nextQuestion = () => {
+    setIndex(oldIndex => {
+      const index = oldIndex + 1;
+      if (index > questions.length - 1) {
+        // openModal()
+        return 0;
+      } else {
+        return index;
+      }
+    });
+  };
+
   useEffect(() => {
     fetchQuestions(tempUrl);
   }, []);
@@ -59,7 +71,8 @@ const AppProvider = ({ children }) => {
         index,
         correct,
         error,
-        isModalOpen
+        isModalOpen,
+        nextQuestion
       }}
     >
       {children}
