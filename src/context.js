eb266955bdb14469f7812ca5281b74de229cadc3
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-17 06:59:43 -0500

    add: fetch questions setup in context

--- src/context.js
+++ src/context.js
@@ -25,6 +25,31 @@ const AppProvider = ({ children }) => {
   const [error, setError] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const fetchQuestions = async url => {
+    setLoading(true);
+    setWaiting(false);
+    const response = await axios(url).catch(err => console.log(err));
+    console.log(response);
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
+
   return (
     <AppContext.Provider
       value={{
